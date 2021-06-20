c2192f444bec0d4e51ccca1b83f42b4691d67614
Anthony Morgan <morganaj@ucmail.uc.edu>
2021-06-20 13:55:42 -0400

    Add BucketItem class

--- app/src/main/java/com/bucketlist/MainActivity.kt
+++ app/src/main/java/com/bucketlist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.bucketlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.bucketlist.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,18 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+        val spinner: Spinner = findViewById(R.id.interests_spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.interests_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
         }
     }
 }
