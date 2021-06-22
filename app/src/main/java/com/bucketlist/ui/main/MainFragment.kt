16db6b27d40d58af28fa4dda10091753a0876176
ayibrahim1 <ibrahiay@mail.uc.edu>
2021-06-22 18:49:55 -0400

    Revert "My Code Review 1"
    This reverts commit 8ad86c36639efc85151d01dffe95ae54ff344ad6.

--- app/src/main/java/com/bucketlist/ui/main/MainFragment.kt
+++ app/src/main/java/com/bucketlist/ui/main/MainFragment.kt
@@ -9,18 +9,26 @@ import android.view.ViewGroup
 import com.bucketlist.R
 
 class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
     private lateinit var viewModel: MainViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-               viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
     }
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+
+
+
 }
--- app/src/main/java/com/bucketlist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/bucketlist/ui/main/MainViewModel.kt
@@ -1,14 +1,17 @@
 package com.bucketlist.ui.main
 
+import android.content.ContentValues.TAG
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bucketlist.dto.BucketListItem
 
 class MainViewModel : ViewModel() {
-    //Use of Array list to get and set bucket list items
     private var _bucketlistitems: MutableLiveData<ArrayList<BucketListItem>> = MutableLiveData<ArrayList<BucketListItem>>()
-    var bucketlistitems:MutableLiveData<ArrayList<BucketListItem>>
+
+    internal var bucketlistitems:MutableLiveData<ArrayList<BucketListItem>>
         get () {return _bucketlistitems}
         set (value) {_bucketlistitems = value}
 }
-
