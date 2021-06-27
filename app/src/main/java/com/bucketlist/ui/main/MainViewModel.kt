daa8c0a7076c4c41375bf467b0f4dc360de9fa00
defiantturtle <sarabarnes078@gmail.com>
2021-06-27 19:35:07 -0400

    Code review chang

--- app/src/main/java/com/bucketlist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/bucketlist/ui/main/MainViewModel.kt
@@ -9,7 +9,8 @@ import androidx.lifecycle.viewModelScope
 import com.bucketlist.dto.BucketListItem
 
 class MainViewModel : ViewModel() {
-    private var _bucketlistitems: MutableLiveData<ArrayList<BucketListItem>> = MutableLiveData<ArrayList<BucketListItem>>()
+    private var _bucketlistitems: MutableLiveData<ArrayList<BucketListItem>> =
+        MutableLiveData<ArrayList<BucketListItem>>()
 
     internal var bucketlistitems:MutableLiveData<ArrayList<BucketListItem>>
         get () {return _bucketlistitems}
