4001129983a7a1435cd6fb7904b32162d83636ee
John Stichweh <54503538+JohnStichweh@users.noreply.github.com>
2021-07-24 12:07:03 -0400

    Added parameter info for BucketListItem data class

--- app/src/main/java/com/bucketlist/dto/BucketListItem.kt
+++ app/src/main/java/com/bucketlist/dto/BucketListItem.kt
@@ -4,7 +4,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName="bucketListItem")
-data class BucketListItem(var name: String = "", @PrimaryKey var itemID: Int = 0,) {
+
+/**
+* @param name: What the bucket list item is called
+* @param itemID: Unique Identifier of this bucket list item.
+* */
+
+data class BucketListItem(var name: String = "", @PrimaryKey var itemID: Int = 0) {
     override fun toString(): String {
         return name
     }
