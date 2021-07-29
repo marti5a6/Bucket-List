019143bdd6447d2637084c0ea9b4e4ee31c7939e
Anthony Morgan <morganaj@ucmail.uc.edu>
2021-07-29 11:17:23 -0400

    Add LocationLiveData - add permissions check in MainFragment

--- app/src/main/java/com/bucketlist/ui/main/LocationLiveData.kt
+++ app/src/main/java/com/bucketlist/ui/main/LocationLiveData.kt
@@ -7,6 +7,7 @@ import com.bucketlist.dto.LocationDetails
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationCallback
 
 class LocationLiveData(context : Context): LiveData<LocationDetails>() {
 
@@ -14,7 +15,7 @@ class LocationLiveData(context : Context): LiveData<LocationDetails>() {
 
     override fun onInactive() {
         super.onInactive()
-        fusedLocationClient.removeLocationUpdates(locationCallBack)
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
     override fun onActive(){
         super.onActive()
@@ -26,13 +27,13 @@ class LocationLiveData(context : Context): LiveData<LocationDetails>() {
     }
 
     private fun startLocationUpdates(){
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallBack, null)
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 
-    private val locationCallBack = object: LocationCallBack() {
+    private val locationCallback = object: LocationCallback() {
        override fun onLocationResult(locationResult: LocationResult?){
             super.onLocationResult(locationResult)
-           locationResult ?: return
+            locationResult ?: return
             for(location in locationResult.locations){
                 setLocationData(location)
             }
