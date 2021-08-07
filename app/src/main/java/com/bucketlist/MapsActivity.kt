7db1be2aced76a1eb13c3e465a314d74bcc41fb9
Johnathan Otten <ottenjg@mail.uc.edu>
2021-08-06 23:37:34 -0400

    final

--- app/src/main/java/com/bucketlist/MapsActivity.kt
+++ app/src/main/java/com/bucketlist/MapsActivity.kt
@@ -88,7 +88,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         for (i in mlocationArray)
         mMap.addMarker(MarkerOptions().position(i).title("Marker in Cincinnati"))
         for (i in mlocationArray)
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(5.0f))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12.0f))
         for (i in mlocationArray)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(i))
     }
